f3752f3d9c1d2e3c1eefb1a10f509cc3a5fe4c3b
martin-minarik <mn.minarik@gmail.com>
2023-07-21 00:36:58 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
