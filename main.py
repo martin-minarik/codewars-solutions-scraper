d786e9cb18936b8537ab57b01d6279a22c29f3d3
martin-minarik <mn.minarik@gmail.com>
2023-07-21 00:44:50 +0200

    prepare_driver method

--- main.py
+++ main.py
@@ -4,6 +4,19 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
+def prepare_driver(chrome_user_data_path, profile):
+    options = webdriver.ChromeOptions()
+    options.add_argument("start-maximized")
+
+    # e.g. C:\Users\<You>\AppData\Local\Google\Chrome\User Data
+    options.add_argument(
+        fr"--user-data-dir={chrome_user_data_path}")
+
+    options.add_argument(fr'--profile-directory={profile}')  # e.g. Profile 3
+
+    return webdriver.Chrome(options=options)
+
+
 def main():
     pass
 
