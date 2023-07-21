7d5ff6da5954c26ed37bf7281e1c08d2bc9c23a9
martin-minarik <mn.minarik@gmail.com>
2023-07-21 23:33:39 +0200

    fixed locators

--- locators.py
+++ locators.py
@@ -6,9 +6,9 @@ solutions_tab = (By.XPATH, "//a[text()='Solutions']")
 h5_loading_more = (By.XPATH, "//div//h5[text()='Loading more items...']")
 
 solution_list_item = (By.XPATH, "//div[@class='list-item-solutions']")
-solution_kata_link = (By.XPATH, "/div[@class='item-title']//a")
-solution_kyu = (By.XPATH, "/div[@class='item-title']/div[contains(@class, 'hex')]/div/span")
-solution_language = (By.XPATH, "/h6")
-solution_code = (By.XPATH, "//pre//code")
+solution_kata_link = (By.XPATH, "./div[@class='item-title']//a")
+solution_kyu = (By.XPATH, ".//div//span[contains(text(), 'kyu') or text()='Retired']")
+solution_language = (By.XPATH, "./h6")
+solution_code = (By.XPATH, ".//pre//code")
 
 kata_description = (By.ID, "description")
